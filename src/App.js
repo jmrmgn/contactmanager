617a457c032afbdde92a11d03f1e2f208e0f3dbb
Jomar <jomar.magno026@gmail.com>
2018-12-28 19:58:59 +0800

    update contact functionality added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 // Browser = Parent, Route = route, Switch = Error page
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Components
 import Header from './components/layout/Header';
 import About from './components/pages/About';
 import Contacts from './components/contacts/Contacts';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 
 // Test Component
 import Test from './components/test/Test';
@@ -33,6 +34,7 @@ class App extends Component {
                         <Route exact path="/" component={Contacts} />
                         <Route exact path="/about" component={About} />
                         <Route exact path="/contact/add" component={AddContact} />
+                        <Route exact path="/contact/edit/:id" component={EditContact} />
                         <Route exact path="/test" component={Test} />
                         <Route component={NotFound} />
                      </Switch>
--- src/components/Contacts/Contact.js
+++ src/components/Contacts/Contact.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -39,6 +40,19 @@ class Contact extends Component {
                         {name}
                         <i className="fas fa-sort-down ml-2 down-arrow" onClick={this.onShowClick} />
                         <i className="fas fa-times delete-arrow" onClick={this.onDeleteClick.bind(this, id, dispatch)} />
+                        <Link to={`/contact/edit/${id}`}>
+                           <i 
+                              className="fas fa-pencil-alt"
+                              style = {
+                                 {
+                                    cursor: 'pointer',
+                                    float: 'right',
+                                    color: 'black',
+                                    marginRight: '1rem'
+                                 }
+                              }
+                           />
+                        </Link>
                      </h4>
                      { showContactInfo ? (
                         <ul className="list-group">
