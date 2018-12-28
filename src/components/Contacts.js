a1a53aef20913455dc3fe3a2ec37175fe03c7566
Jomar <jomar.magno026@gmail.com>
2018-12-28 14:42:35 +0800

    contacts component added

---
+++ src/components/Contacts.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Contact from './Contact';
+
+class Contacts extends Component {
+   state = {
+      contacts: [
+         {
+            id: 1,
+            name: 'John Doe',
+            email: 'jdoe@gmail.com',
+            phone: '555-5555-555'
+         },
+         {
+            id: 2,
+            name: 'Karen Williams',
+            email: 'karen@gmail.com',
+            phone: '123-456-555'
+         },
+         {
+            id: 3,
+            name: 'Henry Johnny',
+            email: 'henry@gmail.com',
+            phone: '000-123-123'
+         }
+      ]
+   };
+
+   render() {
+      const { contacts } = this.state;
+
+      return (
+         <div>
+            {
+               contacts.map(contact => {
+                  return <Contact 
+                     key={contact.id}
+                     contact={contact}
+                  />
+               })
+            }
+         </div>
+      );
+   };
+}
+
+export default Contacts;
