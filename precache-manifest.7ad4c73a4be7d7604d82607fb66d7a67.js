73572cf5bd6d451eb857520a6166e8fe1139b768
Jomar <jomar.magno026@gmail.com>
2018-12-28 20:06:35 +0800

    Updates

---
+++ precache-manifest.7ad4c73a4be7d7604d82607fb66d7a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f66cbd3a0a0539162a91",
+    "url": "/contactmanager/static/css/main.e4cbbf1b.chunk.css"
+  },
+  {
+    "revision": "f66cbd3a0a0539162a91",
+    "url": "/contactmanager/static/js/main.f66cbd3a.chunk.js"
+  },
+  {
+    "revision": "4ac1eb775703eeb30fe7",
+    "url": "/contactmanager/static/css/1.ed0644e3.chunk.css"
+  },
+  {
+    "revision": "4ac1eb775703eeb30fe7",
+    "url": "/contactmanager/static/js/1.4ac1eb77.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9ffd6f8f034f664389834158da58ff5b",
+    "url": "/contactmanager/index.html"
+  }
+];
